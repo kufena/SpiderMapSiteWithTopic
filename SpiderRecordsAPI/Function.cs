f7bfed1e4af4e64eee08b8cc86edb4a962cabe18
Andrew Douglas <fairtomiddlingster@gmail.com>
2022-08-24 17:31:05 +0100

    got the topic working, queue and queue policy to implement the access policy, start on api.

---
+++ SpiderRecordsAPI/Function.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SpiderRecordsAPI;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    {
+        return input.ToUpper();
+    }
+}
